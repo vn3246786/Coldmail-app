59b01bb0e3e97ccc7e313b63716c597bc03eccad
viraj naik <vn3246786@gmail.com>
2024-12-01 01:52:37 +0530

    update

--- Client/src/apiCalls.jsx
+++ Client/src/apiCalls.jsx
@@ -26,7 +26,7 @@ export async function login(dispatch,formData){
 
 dispatch({type:"start"})
 try {
-    const res =await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/auth/login`,formData)
+    const res =await axios.post(`${URL}/api/auth/login`,formData)
     if(findError(res.data)){
         dispatch({type:"failure",payload:res.data})
         toast.error(res.data)
